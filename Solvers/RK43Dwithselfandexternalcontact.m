72bf53086e6292eef0575ba7bd9f22745ab02ced
Luca Di Stasio <luca.distasio@gmail.com>
2016-03-07 11:30:56 +0100

    Added files via upload

---
+++ Solvers/RK43Dwithselfandexternalcontact.m
@@ -0,0 +1,55 @@
+function[lattice,vel]=RK43Dwithselfandexternalcontact(elasticparam,dissipationparam,tn,dt,N,Nx,Ny,lattice,vel,m,ksthandle,kshhandle,kbehandle,dsthandle,dshhandle,dbehandle,fexthandle,boundarysets,boundarytype,posfuncs,velfuncs,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8)
+
+%%
+%        Project: Fluid - structure interaction on deformable surfaces
+%         Author: Luca Di Stasio
+%    Institution: ETH Zürich
+%                 Institute for Building Materials
+% Research group: Computational Physics for Engineering Materials
+%        Version: 0.1
+%  Creation date: August 4th, 2014
+%    Last update: August 4th, 2014
+%
+%    Description: 
+%          Input: 
+%         Output: 
+
+%%
+
+Fn = evaluateelasticforces3D(tn,N,Nx,Ny,lattice,elasticparam,ksthandle,kshhandle,kbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluatedampingforces3D(tn,N,Nx,Ny,lattice,dissipationparam,vel,dsthandle,dshhandle,dbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluateexternalforces3D(tn,lattice,vel,fexthandle) + evaluateselfcontact3D(N,lattice,boundary,Rcsq,ksc) + evaluateexternalcontact3D(t,N,lattice,boundary,boundhandle,gradienthandle,kec);
+
+x1 =  lattice(:,7:9) + 0.5*dt.*vel;
+vel1 = vel + 0.5*dt.*F./m;
+
+[lattice1,vel1] = set_boundaryconditions(tn+dt,N,[lattice(:,1:6) x1],vel1,boundarysets,boundarytype,posfuncs,velfuncs);
+
+F1 = evaluateelasticforces3D(tn+0.5*dt,N,Nx,Ny,lattice1,elasticparam,ksthandle,kshhandle,kbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluatedampingforces3D(tn+0.5*dt,N,Nx,Ny,lattice1,dissipationparam,vel1,dsthandle,dshhandle,dbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluateexternalforces3D(tn+0.5*dt,lattice1,vel1,fexthandle) + evaluateselfcontact3D(N,lattice1,boundary,Rcsq,ksc) + evaluateexternalcontact3D(t,N,lattice1,boundary,boundhandle,gradienthandle,kec);
+
+x2 =  lattice(:,7:9) + 0.5*dt.*vel1;
+vel2 = vel + 0.5*dt.*F1./m;
+
+[lattice2,vel2] = set_boundaryconditions(tn+dt,N,[lattice(:,1:6) x2],vel2,boundarysets,boundarytype,posfuncs,velfuncs);
+
+F2 = evaluateelasticforces3D(tn+0.5*dt,N,Nx,Ny,lattice2,elasticparam,ksthandle,kshhandle,kbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluatedampingforces3D(tn+0.5*dt,N,Nx,Ny,lattice2,dissipationparam,vel2,dsthandle,dshhandle,dbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluateexternalforces3D(tn+0.5*dt,lattice2,vel2,fexthandle) + evaluateselfcontact3D(N,lattice2,boundary,Rcsq,ksc) + evaluateexternalcontact3D(t,N,lattice2,boundary,boundhandle,gradienthandle,kec);
+
+x3 =  lattice(:,7:9) + dt.*vel2;
+vel3 = vel + dt.*F2./m;
+
+[lattice3,vel3] = set_boundaryconditions(tn+dt,N,[lattice(:,1:6) x3],vel3,boundarysets,boundarytype,posfuncs,velfuncs);
+
+F3 = evaluateelasticforces3D(tn+dt,N,Nx,Ny,lattice3,elasticparam,ksthandle,kshhandle,kbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluatedampingforces3D(tn+dt,N,Nx,Ny,lattice3,dissipationparam,vel3,dsthandle,dshhandle,dbehandle,indicesbulk,indicesinternalbulk,indicesF1,indicesF2,indicesF3,indicesF4,indicesF5,indicesF6,indicesinternalF1,indicesinternalF2,indicesinternalF3,indicesinternalF4,indicesinternalF5,indicesinternalF6,indicesE1,indicesE2,indicesE3,indicesE4,indicesE5,indicesE6,indicesE7,indicesE8,indicesE9,indicesE10,indicesE11,indicesE12,indicesinternalE1,indicesinternalE2,indicesinternalE3,indicesinternalE4,indicesinternalE5,indicesinternalE6,indicesinternalE7,indicesinternalE8,indicesinternalE9,indicesinternalE10,indicesinternalE11,indicesinternalE12,indicesC1,indicesC2,indicesC3,indicesC4,indicesC5,indicesC6,indicesC7,indicesC8,indicesinternalC1,indicesinternalC2,indicesinternalC3,indicesinternalC4,indicesinternalC5,indicesinternalC6,indicesinternalC7,indicesinternalC8) + ...
+    evaluateexternalforces3D(tn+dt,lattice3,vel3,fexthandle) + evaluateselfcontact3D(N,lattice3,boundary,Rcsq,ksc) + evaluateexternalcontact3D(t,N,lattice3,boundary,boundhandle,gradienthandle,kec);
+
+lattice(:,7:9) =  lattice(:,7:9) + (1/6)*dt.*(vel + 2.*vel1 + 2.*vel2 + vel3);
+vel = vel + (1/6)*dt.*(Fn + 2.*F1 + 2.*F2 + F3)./m;
+
+[lattice,vel] = set_boundaryconditions(tn+dt,N,lattice,vel,boundarysets,boundarytype,posfuncs,velfuncs);
+
+return
